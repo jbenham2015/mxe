c02e72abd928bc06e4fd4446522bf5f639119000
Jeremiah Benham <jjbenham@chicagoguitar.com>
2026-03-14 19:53:15 +0000

    Fix denemo tarball URL to use https

--- src/denemo.mk
+++ src/denemo.mk
@@ -3,10 +3,10 @@
 PKG             := denemo
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.6.51
-$(PKG)_CHECKSUM := e623b4061b3b6122b6db996e12d9df3324e140435d04ae55b4a6eec7f67cc6eb
+$(PKG)_CHECKSUM := 8ea8e546c55e27b540fee0b8af328e558fec75bade29730e9cc9d195757ce899
 $(PKG)_SUBDIR   := denemo-$($(PKG)_VERSION)
 $(PKG)_FILE     := denemo-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://www.denemo.org/~jjbenham/denemo-snapshot/$($(PKG)_FILE)
+$(PKG)_URL      := https://denemo.org/~jjbenham/denemo-snapshot/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc gtk3 gtksourceview aubio portaudio librsvg libgcrypt portmidi libsndfile evince rubberband fluidsynth  
 
 #TODO portmidi rubnerband path
@@ -15,7 +15,7 @@ $(PKG)_DEPS     := gcc gtk3 gtksourceview aubio portaudio librsvg libgcrypt port
 #z%TODO write test for aubio
 #TODO write test for evince 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://www.denemo.org/~jjbenham/denemo-snapshot/' | \
+    $(WGET) -q -O- 'https://denemo.org/~jjbenham/denemo-snapshot/' | \
     grep 'denemo-' | \
    $(SED) -n 's,.*denemo-\([0-9][^>]*\)\.tar.*,\1,p' | \
 sort | \
@@ -46,16 +46,3 @@ define $(PKG)_BUILD
 
 endef
 
-
-#define $(PKG)_BUILD
-#    cd '$(1)/' && ./autogen.sh \
-#    cd '$(1)/' && ./configure \
-#        $(MXE_CONFIGURE_OPTS) \
-#        --disable-binreloc \
-#	--enable-guile_1_8 \
-#	--enable-portmidi \
-#	--enable-portaudio \
-#	--disable-rubberband \
-#        CFLAGS="-mwindows -D_GUB_BUILD_"
-#    $(MAKE) -C '$(1)/' -j '$(JOBS)' AM_LDFLAGS="-mwindows" LDFLAGS="-lportmidi" install
-#endef
