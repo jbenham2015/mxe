2a397f616d7b17f3bcb394810e269fe7e114ebec
Jeremiah Benham <jjbenham@chicagoguitar.com>
2026-05-15 15:07:05 +0000

    explicitly inherit standard handles in wrapper

--- src/lilypond-windows.c
+++ src/lilypond-windows.c
@@ -1,7 +1,6 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     STARTUPINFOA si;
@@ -9,29 +8,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     char cmdline[4096];
     char exepath[MAX_PATH];
     DWORD exitcode;
-
     (void)hInstance;
     (void)hPrevInstance;
     (void)nCmdShow;
-
-    /* Find lilypond.exe in the same directory as this wrapper */
     GetModuleFileNameA(NULL, exepath, MAX_PATH);
     strrchr(exepath, '\\')[1] = '\0';
     strncat(exepath, "lilypond.exe", MAX_PATH - strlen(exepath) - 1);
-
     if (lpCmdLine && lpCmdLine[0] != '\0')
         snprintf(cmdline, sizeof(cmdline), "\"%s\" %s", exepath, lpCmdLine);
     else
         snprintf(cmdline, sizeof(cmdline), "\"%s\"", exepath);
-
     memset(&si, 0, sizeof(si));
     si.cb = sizeof(si);
+    si.dwFlags = STARTF_USESTDHANDLES;
+    si.hStdInput  = GetStdHandle(STD_INPUT_HANDLE);
+    si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    si.hStdError  = GetStdHandle(STD_ERROR_HANDLE);
     memset(&pi, 0, sizeof(pi));
-
     if (!CreateProcessA(NULL, cmdline, NULL, NULL, TRUE,
                         CREATE_NO_WINDOW, NULL, NULL, &si, &pi))
         return 1;
-
     WaitForSingleObject(pi.hProcess, INFINITE);
     GetExitCodeProcess(pi.hProcess, &exitcode);
     CloseHandle(pi.hProcess);
